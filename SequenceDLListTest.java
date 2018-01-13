1366ebe1006d5a0b24473bc86d31f5b16b63ce73
FlorianObst <FlorianObst@users.noreply.github.com>
2018-01-13 11:38:48 +0100

    Add files via upload

---
+++ SequenceDLListTest.java
@@ -0,0 +1,18 @@
+
+public class SequenceDLListTest {
+  public static void main(String[] args) {
+    SequenceDLList<Integer> myList = new SequenceDLList<Integer>();
+    
+    for (int i=0; i<26; i++) {
+      myList.insertFirst(new Integer(i));
+    }
+    
+    while(!myList.empty())
+      try {
+        System.out.println(myList.first());
+        myList.deleteFirst();
+      } catch (SequenceException e) {
+        System.out.println(e);
+      }
+  }
+}
